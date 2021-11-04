3afac29ffe38b34d55dcf1cac30b8bf94bf91f97
vitorstraggiotti <vitorstraggiotti@hotmail.com>
2021-11-04 17:19:54 -0300

    .

--- sha256.h
+++ sha256.h
@@ -9,8 +9,9 @@
 
 #define SHA256_VERBOSE		1
 #define SHA256_NOT_VERBOSE	0
+
 /******************************************************************************/
-//Return 32 bytes digest of Data on success. Return NULL if fail.
+//Return 32 bytes digest of Data array on success. Return NULL if fail.
 //VerboseStatus = SHA256_VERBOSE --> Will print progress
 //VerboseStatus = SHA256_NOT_VERBOSE --> Will not print progress
 uint8_t *sha256_data(uint8_t *Data, uint64_t DataSizeByte, uint8_t VerboseStatus);
